23a8be6f1d5caebb565aac79db4782ba015afff1
Ameya <adengane@gmai.com>
2023-01-21 12:03:46 +0530

    added module css to all components

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 import styled from 'styled-components'
 import Navbar from './components/Navbar/Navbar'
 import Mint from './components/Mint/Mint'
-import Redeem from './components/Reedem/Reedem'
+import Redeem from './components/Redeem/Redeem'
 
 function App() {
   const [count, setCount] = useState(0)
